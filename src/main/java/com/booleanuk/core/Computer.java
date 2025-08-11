53447e37e57a23c0b3d99527aaf4036a35f6bd66
Linda Do <lindado_98@hotmail.com>
2025-08-11 09:26:33 +0200

    extension completed

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -20,8 +20,7 @@ public class Computer {
         this.psu.turnOn();
     }
 
-    public void installGame(String gameName) {
-        Game game = new Game(gameName);
+    public void installGame(Game game) {
         this.installedGames.add(game);
     }
 
