896df7a9dc73ee87fdecb55da75cdcdf873af8f9
Linda Do <lindado_98@hotmail.com>
2025-08-11 09:22:52 +0200

    core completed

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,21 +3,31 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
-    public ArrayList<Game> installedGames = new ArrayList<>();
+    public ArrayList<Game> installedGames;
+    PowerSupply psu;
 
-    public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+    public Computer(PowerSupply psu){
+        this.psu = psu;
+        installedGames = new ArrayList<>();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public Computer(PowerSupply psu, ArrayList<Game> preInstalled){
+        this.psu = psu;
+        this.installedGames = preInstalled;
+    }
+
+    public void turnOn() {
+        this.psu.turnOn();
+    }
+
+    public void installGame(String gameName) {
+        Game game = new Game(gameName);
         this.installedGames.add(game);
     }
 
-    public String playGame() {
+    public String playGame(String game) {
         for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
+            if (g.name.equals(game)) {
                 return g.start();
             }
         }
